c4e65792ec96c07cd87f2bd14acbc9633e812ae9
alexhordynsky <hordynskyya@gmail.com>
2019-11-07 09:22:07 +0100

    done

--- Open-Lab-03.04/Numbers.cs
+++ Open-Lab-03.04/Numbers.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_03._04
     {
         public string EvenOrOdd(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+                return "even";
+            else
+            {
+                return "odd";
+            }
+
         }
     }
 }
